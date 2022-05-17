8648d8231801cfc633c4356df875be30adcf6fd7
Jakob McBroome <jmcbroome@gmail.com>
2022-05-16 18:51:24 -0700

    update figures again

--- do_benchmarking.py
+++ do_benchmarking.py
@@ -6,11 +6,11 @@ import sys
 #because of how memory is used and freed by the subprocesses. This ensures that memory used in generating
 #subtrees is always completely freed.
 
-subprocess.check_call("python3 create_random_subsets.py public-2022-04-08.all.masked.nextclade.pangolin.pb.gz 1 6 10",shell=True)
+subprocess.check_call("python3 create_random_subsets.py public-2022-04-08.all.masked.nextclade.pangolin.pb.gz 3 6 6",shell=True)
 sizes = [int(s.strip()) for s in open("subsets/sizes_saved.txt").readlines()]
 alldf = []
 try:
-    for p in range(0):
+    for p in range(5):
         for s in sizes:
             print("Benchmarking subtree size {}...".format(s),file=sys.stderr)
             subprocess.check_call("python3 bench_loading.py subsets/{}_random.pb".format(s),shell=True)
@@ -20,15 +20,15 @@ try:
             #removing the find benchmark for now because everything is really fast and its less informative.
             #subprocess.check_call("python3 bench_find.py subsets/{}_random.pb".format(s),shell=True)
             #load the resulting dataframes and concatenate them
-            df = pd.read_csv("traversal_times.csv")
+            df1 = pd.read_csv("traversal_times.csv")
             # df2 = pd.read_csv("subtree_times.csv")
             df3 = pd.read_csv("load_times.csv")
             df4 = pd.read_csv("rsearch_times.csv")
-            df = reduce(lambda left,right: pd.merge(left,right,on=['Package','TreeSize'], how='outer'), [df,df3,df4])
+            df = reduce(lambda left,right: pd.merge(left,right,on=['Package','TreeSize'], how='outer'), [df1,df3,df4])
             #df['Perm'] = p
             alldf.append(df)
 except KeyboardInterrupt:
     pass
 # df_merged = reduce(lambda left,right: pd.merge(left,right,on=['Package','TreeSize'], how='outer'), alldf)
 df_merged = pd.concat(alldf,axis=0)
-df_merged.to_csv("benchmarking_results_profiled_2.csv",index=False)
+df_merged.to_csv("benchmarking_results_profiled_3.csv",index=False)
